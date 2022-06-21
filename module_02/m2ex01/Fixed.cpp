9962f07008f20edf86ff4c0d9d112cf702bafe75
lisa_codam <lisa.vlamings@gmail.com>
2022-06-21 13:28:20 +0200

    begin module 02

--- module_02/m2ex01/Fixed.cpp
+++ module_02/m2ex01/Fixed.cpp
@@ -10,6 +10,14 @@ Fixed::Fixed(void){
     std::cout << "Default constructor called"<< std::endl;
 }
 
+Fixed::Fixed(const int val){
+    this->_FixedPointVal = val;
+}
+
+Fixed::Fixed(const float val){
+    this->_FixedPointVal = (int)val;
+}
+
 Fixed::Fixed(const Fixed &copy){
     std::cout << "Copy constructor called" << std::endl;
     *this = copy;
@@ -21,6 +29,12 @@ Fixed& Fixed::operator=(const Fixed &copy){
     return (*this);
 }
 
+std::ostream &operator<<(std::ostream &out, const Fixed &copy)
+{
+    out << copy.toFloat();
+    return (out);
+}
+
 int Fixed::getRawBits( void ) const {
     std::cout << "getRawBits member function called" << std::endl;
     return(this->_FixedPointVal);
@@ -29,6 +43,16 @@ int Fixed::getRawBits( void ) const {
 void Fixed::setRawBits( int const raw ){
     std::cout << "setRawBits member function called" << std::endl;
     this->_FixedPointVal = raw;
+}
+
+float  Fixed::toFloat( void ) const{
+    return(this->_FixedPointVal);
+//    fixed point to floating point val
+}
+
+int Fixed::toInt( void ) const{
+    return ((int)this->_FixedPointVal);
+//    converts the fixed-point value to an integer value.
 }
 
 Fixed::~Fixed(void){
