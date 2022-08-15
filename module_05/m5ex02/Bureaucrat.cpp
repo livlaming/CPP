c170620e0872ede60bc35915c9cf30f821dc1c36
lisa_codam <lisa.vlamings@gmail.com>
2022-08-15 11:47:57 +0200

    ex01

---
+++ module_05/m5ex02/Bureaucrat.cpp
@@ -0,0 +1,67 @@
+//
+// Created by Lisa Vlamings on 8/10/22.
+//
+
+#include "Bureaucrat.hpp"
+#include "Form.hpp"
+
+Bureaucrat::Bureaucrat(){};
+
+Bureaucrat::Bureaucrat(const std::string Name, int grade) : _name(Name) {
+    this->_grade = grade;
+    if (this->_grade < 1 )
+        throw Bureaucrat::GradeTooHighException();
+    else if (this->_grade > 150)
+        throw Bureaucrat::GradeTooLowException();
+}
+
+
+Bureaucrat::Bureaucrat(const Bureaucrat &copy) : _name(copy._name) {
+    std::cout << "Copy constructor Cat called" << std::endl;
+    *this = copy;
+}
+
+Bureaucrat &Bureaucrat::operator=(const Bureaucrat &copy) {
+    std::cout << "Copy assignment operator Cat called" << std::endl;
+    this->_grade = copy.getGrade();
+    return (*this);
+}
+
+int Bureaucrat::getGrade() const{
+    return(this->_grade);
+}
+
+std::string Bureaucrat::getName() const {
+    return (this->_name);
+}
+
+void Bureaucrat::increment() {
+    this->_grade -= 1;
+    if (this->_grade < 1)
+        throw Bureaucrat::GradeTooHighException();
+}
+
+void Bureaucrat::decrement() {
+    this->_grade += 1;
+    if (this->_grade > 150 )
+        throw Bureaucrat::GradeTooLowException();
+}
+
+std::ostream &operator<<(std::ostream &out, Bureaucrat &copy)
+{
+    out << copy.getName() <<", bureaucrat grade " << copy.getGrade() << "." << std::endl;
+    return (out);
+}
+
+void Bureaucrat::signForm(Form &copy){
+    try{
+        copy.beSigned(*this);
+        std::cout << copy.getName() << " signed " << this->getName() << std::endl;
+    }
+    catch(const std::exception& Exc)
+    {
+        std::cout << copy.getName() << " couldn't sign " << this->getName() << " because " << Exc.what() << std::endl;
+    }
+}
+
+Bureaucrat::~Bureaucrat() {}
---
+++ module_05/m5ex02/Form.cpp
@@ -0,0 +1,57 @@
+//
+// Created by Lisa Vlamings on 8/12/22.
+//
+
+#include "Form.hpp"
+#include "Bureaucrat.hpp"
+
+Form::Form() : _name(NULL), _grade_sign(0), _grade_exc(0), _signed(false){}
+
+Form::Form(const std::string Name, const int GradeTS, const int GradeTE) : _name(Name), _grade_sign(GradeTS), _grade_exc(GradeTE), _signed(false){}
+
+Form::Form(const Form &copy) : _name(copy.getName()), _grade_sign(copy.getGradeSign()), _grade_exc(copy.getGradeExc()) {
+    std::cout << "Copy constructor Cat called" << std::endl;
+    *this = copy;
+}
+
+Form &Form::operator=(const Form &copy) {
+    std::cout << "Copy assignment operator Cat called" << std::endl;
+    this->_signed = copy.getSign();
+    return (*this);
+}
+
+std::string Form::getName() const {
+    return(this->_name);
+}
+
+int Form::getGradeSign() const {
+    return(this->_grade_sign);
+}
+
+int Form::getGradeExc() const {
+    return(this->_grade_exc);
+}
+
+bool Form::getSign() const{
+    return(this->_signed);
+}
+
+std::ostream &operator<<(std::ostream &out, Form &copy)
+{
+    out << "Name: " <<  copy.getName() << std::endl;
+    out << "Signed?: " << copy.getSign() << std::endl;
+    out << "Grade to sign: " << copy.getGradeSign() << std::endl;
+    out << "Grade to Execute: " << copy.getGradeExc() << std::endl;
+    return (out);
+}
+
+void Form::beSigned(const Bureaucrat &copy){
+    if (copy.getGrade() > this->_grade_sign)
+        throw Form::GradeTooLowException();
+    else if (copy.getGrade() < 1)
+        throw Form::GradeTooHighException();
+    else
+        this->_signed = true;
+}
+
+Form::~Form(){}
