ec8729184dc0c782a86c54132f9fbceb36953751
lisa_codam <lisa.vlamings@gmail.com>
2022-08-15 11:04:30 +0200

    ex01 af

--- module_05/m5ex01/main.cpp
+++ module_05/m5ex01/main.cpp
@@ -6,20 +6,24 @@
 #include "Form.hpp"
 
 int main() {
+
+    std::cout << "\n----------------\n" << std::endl;
     try {
         Bureaucrat She("Lola", 5);
-        Form College("entrance programme X", 6, 6);
-        She.signForm(College);
+        std::cout << She << std::endl;
+        Form College("Entrance programme X", 6, 6);
         std::cout << College << std::endl;
-
+        She.signForm(College);
+        std::cout << std::endl;
+        std::cout << College << std::endl;
     }
     catch (std::exception &Exc) {
         std::cout << Exc.what() << std::endl;
     }
-
+    std::cout << "\n----------------\n" << std::endl;
     try {
         Bureaucrat He("George", 7);
-        Form College2("entrance programme Y", 5, 5);
+        Form College2("Entrance programme Y", 5, 5);
         He.signForm(College2);
         std::cout << College2 << std::endl;
     }
@@ -27,6 +31,7 @@ int main() {
         std::cout << Exc.what() << std::endl;
     }
 
+    system("leaks ex01");
 
     return 0;
 }
