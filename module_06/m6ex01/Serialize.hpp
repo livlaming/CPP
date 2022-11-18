dc52b9985eb6724b0c077c697d423f98f9d97a8b
Lisa Vlamings <livlamin@f0r3s22.codam.nl>
2022-11-18 15:03:02 +0100

    ex01:

---
+++ module_06/m6ex01/Serialize.hpp
@@ -0,0 +1,15 @@
+#ifndef CPP_SERIALIZE_HPP
+#define CPP_SERIALIZE_HPP
+
+#include <iostream>
+//#include <stdint.h>
+
+struct Data{
+    std::string str;
+    int         num;
+};
+
+uintptr_t   serialize(Data *ptr);
+Data *deserialize(uintptr_t raw);
+
+#endif //CPP_SERIALIZE_HPP
