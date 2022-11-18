1e00b79a90b268544fe21b9aca5064d2ba199ca2
Lisa Vlamings <livlamin@f0r3s22.codam.nl>
2022-11-18 18:26:36 +0100

    7 ex01 not done

--- module_07/m7ex01/iter.hpp
+++ module_07/m7ex01/iter.hpp
@@ -1,15 +1,13 @@
-//
-// Created by Lisa Vlamings on 11/18/22.
-//
-
 #ifndef CPP_ITER_HPP
 #define CPP_ITER_HPP
 
-template <typename T> // this is the template parameter declaration
-void iter(T Address, T Length, T Function) // this is the function template definition for max<T>
+#include <iostream>
+template <typename T, typename F>
+void iter(T *A, size_t len, void(*f)(F&))
 {
-    return;
+    for(int i;i<len;len++){
+        f(A[i]);
+    }
 }
-
 
 #endif //CPP_ITER_HPP
--- module_07/m7ex01/main.cpp
+++ module_07/m7ex01/main.cpp
@@ -1,8 +1,24 @@
 #include "iter.hpp"
+#include <iostream>
 
-int main(void){
+template <typename T>
+void costumPrint(T const &x) {
+    std::cout << x << std::endl;
+}
 
-    iter()
+int	main()
+{
+    int	tab[] = { 0, 1, 2, 3, 4 };
+//    float       f[] = {1,1.0, 3.4,7.7};
+//    std::string s[] = {"hi", "hello", "hai", "hoi", "hallo"};
+
+    size_t five = 5;
+    ::iter(tab, 5, costumPrint);
+//    ::iter(f, five, costumePrint);
+//    ::iter(s, 5, costumePrint);
+//    ::iter(f, 5, costumePrint);
 
     return (0);
 }
+
+
