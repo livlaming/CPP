75d2919dea830104fabcc675ab72ba6cba975175
lisa vlamings <>
2022-11-19 14:05:42 +0100

    set up module 8

--- module_08/m8ex01/Span.cpp
+++ module_08/m8ex01/Span.cpp
@@ -1,1 +1,26 @@
 #include "Span.hpp"
+
+Span::Span(){}
+
+Span::Span(unsigned int N) {
+    _N = N;
+}
+
+Span::Span(const Span &copy){
+    if (this != copy){
+
+    }
+}
+
+Span &Span::operator=(const Span &copy){
+    if (this != &copy){
+
+    }
+    return *this;
+}
+
+void    Span::addNumber(){
+    if
+}
+
+~Span::Span(){}
--- module_08/m8ex01/Span.hpp
+++ module_08/m8ex01/Span.hpp
@@ -3,8 +3,20 @@
 
 
 class Span {
+private:
+    unsigned int _N; // max variables
+    Span();
 
+public:
+    Span(unsigned int N);
+    ~Span();
+    Span(const Span &copy);
+    Span &operator=(const Span &copy);
+
+    void    addNumber();
+    void    shortestSpan();
+    void    longestSpan();
 };
-
+//<<ostream
 
 #endif //CPP_SPAN_HPP
