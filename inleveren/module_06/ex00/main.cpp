b8c34f3a0e39ab8a170af428ecbfba9b65edadbe
Lisa Vlamings <livlamin@f0r3s22.codam.nl>
2022-11-24 14:07:23 +0100

    m06

---
+++ inleveren/module_06/ex00/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "Convert.hpp"
+
+
+// Literals are unnamed values inserted directly into the code.
+
+int main (int ac, char const **input){
+    if (ac == 2){
+        input++;
+        Convert literal(*input);
+        std::cout << literal << std::endl;
+    }
+    else
+        std::cout << "Invalid input" << std::endl;
+    return (0);
+}
---
+++ inleveren/module_06/ex01/main.cpp
@@ -0,0 +1,31 @@
+#include "Serialize.hpp"
+
+//
+//void printData(Data *ptr){
+//    std::cout << "Data num: " << ptr->num << std::endl;
+//    std::cout << "Data str: "<< ptr->str << std::endl;
+//    std::cout << std::endl;
+//}
+
+
+int main(void) {
+    Data		*ptr = new Data;
+    Data		*res;
+
+    ptr->str = "string";
+    ptr->num = 1;
+
+    std::cout << "ORIGINAL POINTER: [" << ptr << "]\n" << std::endl;
+
+    uintptr_t	raw = serialize(ptr);
+    std::cout << "RAW BYTES: [" << raw << "]" << std::endl;
+//    printData(ptr);
+
+    res = deserialize(raw);
+    std::cout << "DESERIALIZED: [" << res << "]" << std::endl;
+//    printData(ptr);
+
+    delete ptr;
+    return 0;
+}
+
