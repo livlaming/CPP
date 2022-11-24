d2da7add65aede7d3203647899d72e31c92d1cfb
Lisa Vlamings <livlamin@f0r3s22.codam.nl>
2022-11-24 15:33:33 +0100

    ex02

--- module_06/m6ex02/main.cpp
+++ module_06/m6ex02/main.cpp
@@ -37,32 +37,54 @@ void identify(Base* p){
 
 void identify(Base& p){
     try {
-        A &Aa = dynamic_cast<A &>(p);
+        A &Aa = dynamic_cast<A&>(p);
         std::cout << 'A' << std::endl;
+        (void)Aa;
 
         B &Bb = dynamic_cast<B&>(p);
         std::cout << 'B' << std::endl;
+        (void)Bb;
 
         C &Cc= dynamic_cast<C&>(p);
         std::cout << 'C' << std::endl;
+        (void)Cc;
     }
     catch (std::exception& e) {}
 }
 
-//dynamic casting: by far the most common use for dynamic casting is for converting base-class pointers into derived-class pointers. This process is called downcasting.
+//dynamic casting: by far the most common use for dynamic casting
+// is for converting base-class pointers into derived-class pointers.
+// This process is called downcasting.
 int main(void){
     srand(time(NULL));
+
     A *Aa = new A;
     B Bb;
     C *Cc = new C;
+
     A AA;
 
+    Base *Base_A = new A;;
+    Base *Base_B = new B;
+    Base *Base_C = new C;
+
+    std::cout << "generate random class & identify: " << std::endl;
     Base *res = generate();
     identify(res);
     identify(&(*res));
+
+    std::cout << "\nrandom test: " << std::endl;
     identify(Cc);
     identify(Aa);
+
     identify(&Bb);
     identify(&AA);
+
+    std::cout << "\nBASE TESTS:" << std::endl;
+    identify(Base_A);
+    identify(Base_B);
+    identify(Base_C);
+    std::cout  << std::endl;
+
     return (0);
 }
