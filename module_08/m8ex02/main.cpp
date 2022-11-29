a0e0d27f55ef5c6cc23713cae49fd292ed2801b8
Lisa Vlamings <livlamin@f0r3s22.codam.nl>
2022-11-29 16:00:33 +0100

    module 08 bijna af

--- module_08/m8ex02/main.cpp
+++ module_08/m8ex02/main.cpp
@@ -1,6 +1,19 @@
-#include <iostream>
-#include "MutantStack.hpp"
 
+#include "MutantStack.hpp"
+#include <iostream>
+
+template <typename T>
+void    printStack(MutantStack<T> mstack) {
+    typename MutantStack<T>::iterator it = mstack.begin();
+    typename MutantStack<T>::iterator ite = mstack.end();
+    ++it;
+    --it;
+    while (it != ite) {
+        std::cout << *it << std::endl;
+        ++it;
+    }
+}
+//Container adapters
 void testExample(){
     MutantStack<int> mstack;
     mstack.push(5);
@@ -25,4 +38,48 @@ void testExample(){
     std::stack<int> s(mstack);
 //    find function is cool
     return;
+}
+
+void ownExample(){
+    MutantStack<int> mstack;
+
+    std::cout << "empty? : " << mstack.empty() << std::endl;   //    Test whether container is empty (public member function)
+    mstack.push(2);
+    mstack.push(10);
+    std::cout << "empty? : " <<  mstack.empty() << std::endl;   //    Test whether container is empty (public member function)
+    std::cout << "----------------------" << std::endl;
+    std::cout << "top : " << mstack.size() << std::endl; //    Return size (public member function)
+    std::cout << "----------------------" << std::endl;
+    std::cout << "size : " << mstack.top() << std::endl; //    Access next element (public member function)
+
+    MutantStack<int> mstack2;
+    mstack2.push(4);
+    mstack2.push(20);
+
+    std::cout << "print mstack2" << std::endl;
+    printStack<int>(mstack2);
+    mstack.swap(mstack2); //    Swap contents (public member function)
+    std::cout << "----------------------" << std::endl;
+    std::cout << "print mstack2 after swapping:" << std::endl;
+    printStack<int>(mstack2);
+    std::cout << "----------------------" << std::endl;
+    std::cout << std::endl;
+
+    mstack.pop(); //    Remove top element (public member function)
+}
+
+//If you run it a first time with your MutantStack, and a second time replacing the
+//MutantStack with, for example, a std::list, the two outputs should be the same. Of
+//        course, when testing another container, update the code below with the corresponding
+//        member functions (push() can become push_back())
+
+void    l(){
+    system("leaks ex02");
+}
+
+int main(){
+//    atexit(l);
+//    testExample();
+    ownExample();
+    return (0);
 }
