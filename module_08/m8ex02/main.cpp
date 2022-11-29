1c9a31448fdc406a8b49476cef87111abf8ec08d
Lisa Vlamings <livlamin@f0r3s22.codam.nl>
2022-11-29 18:08:22 +0100

    test remove from main

--- module_08/m8ex02/main.cpp
+++ module_08/m8ex02/main.cpp
@@ -117,11 +117,6 @@ void    reverseTest(){
     printReverseStack<std::string>(mstack2);
 }
 
-//If you run it a first time with your MutantStack, and a second time replacing the
-//MutantStack with, for example, a std::list, the two outputs should be the same. Of
-//        course, when testing another container, update the code below with the corresponding
-//        member functions (push() can become push_back())
-
 void    l(){
     system("leaks ex02");
 }
